7dc2e76a8d94b0a0cda3dc22a85eb98ed1559be0
minseojo <64322765+minseojo@users.noreply.github.com>
2021-08-27 00:42:49 +0900

    Create cube.py

---
+++ cube.py
@@ -0,0 +1,26 @@
+import pyautogui
+import time
+import keyboard
+from random import *
+
+sign=0
+xsize=0
+ysize=0
+while 1:
+    if sign==0:
+        sign=1
+        time.sleep(3)
+    xsize=300
+    ysize=440
+    ysize+= randint(1,10)
+    xsize+= randint(50,100)
+    pyautogui.doubleClick(x=xsize,y=ysize)
+    pyautogui.press('enter')
+    pyautogui.press('enter') 
+    pyautogui.press('enter')
+    pyautogui.doubleClick(x=xsize,y=ysize-50)
+    time.sleep(1.5)
+    if keyboard.is_pressed('q'):
+        print("끝")
+        break
+
